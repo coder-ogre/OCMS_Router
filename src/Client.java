dcd4ae6737a86c5adaedbd2ef298dcfa22003dac
tc9538@cs.ship.edu <tc9538@cs.ship.edu>
2017-04-02 14:44:49 -0400

    Added some comments to the client.

--- src/Client.java
+++ src/Client.java
@@ -17,15 +17,15 @@ import java.util.concurrent.TimeUnit;
 public class Client extends Thread
 {	
 	private Socket connect;
-	/*
-	 * constructor for Basic Client, includes code to be executed upon instantiation
+	/**
+	 * Constructor for Basic Client, includes code to be executed upon instantiation
 	 */
 	public Client() throws IOException, InterruptedException
 	{	
-		/*
-		 * Suppose the client knows all the IP addresses of all routers
-		 */
+		//Suppose the client knows all the IP addresses of all routers
+		//May need to hard code the IP address and write the random selection code
 		String IPAddressDestination ="127.0.0.1";
+		
 		
 		Socket connect = new Socket(IPAddressDestination, 4446); // attempts to ring the bell of this socket address, 127.0.0.1:4446
 		System.out.println("Welcome to 127.0.0.1!");
@@ -37,22 +37,19 @@ public class Client extends Thread
 //			System.out.print("You've entered " + message.length() + " characters as input.\nMake sure your input is exactly 10 characters long, please retry entering. \n");
 //			message = scan.nextLine(); // tries to get input again
 //		}
+		
 		int dataContent = 1;
 		while(dataContent!=10001)
 		{
 			TimeUnit.SECONDS.sleep(2);
-			/**
-			 * Send out message
-			 */
+			//Send out message
 			PrintWriter out = new PrintWriter(connect.getOutputStream(), true); 
 			out.println(dataContent); // sends out message to server
 			
 			//Print out the message and destination IP Address
 			System.out.println("Sending data content is "+ dataContent+" to"+ IPAddressDestination);
 			
-			/**
-			 * Receive message back
-			 */
+			//Receive message back and print it out
 			BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
 			System.out.println("The received message is " + in.readLine() + ".\n"); // gets the reversed message from the server
 			
