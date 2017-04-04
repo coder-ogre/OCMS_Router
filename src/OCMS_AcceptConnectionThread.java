bc18ec46198e1694652f920ad179ec7c14b6257b
Drew Misicko <am8702@cs.ship.edu>
2017-04-04 02:47:40 -0400

    added checksum to router
    From Jessica's checksum checker code :)

--- src/OCMS_AcceptConnectionThread.java
+++ src/OCMS_AcceptConnectionThread.java
@@ -41,26 +41,29 @@ import java.net.Socket;
                 System.out.println(message);
 
                 int nextRouter;
-
-                if((int)message.charAt(2) == id)
+                
+                if(checkChecksum(message))
                 {
-                    nextRouter = id;
-                    connectOut = new Socket(master.getIp(nextRouter), 4446); // attempts to ring the bell of this socket address
-                }
-                else if((int)message.charAt(2) == master.getLeftConnection(id))
-                {
-                    nextRouter = master.getLeftConnection(id);
-                    connectOut = new Socket(master.getIp(nextRouter), 3336); // attempts to ring the bell of this socket address
-                }
-                else
-                {
-                    nextRouter = master.getRightConnection(id);
-                    connectOut = new Socket(master.getIp(nextRouter), 4446); // attempts to ring the bell of this socket address
-                }
+                	if((int)message.charAt(2) == id)
+                    {
+                        nextRouter = id;
+                        connectOut = new Socket(master.getIp(nextRouter), 4446); // attempts to ring the bell of this socket address
+                    }
+                    else if((int)message.charAt(2) == master.getLeftConnection(id))
+                    {
+                        nextRouter = master.getLeftConnection(id);
+                        connectOut = new Socket(master.getIp(nextRouter), 3336); // attempts to ring the bell of this socket address
+                    }
+                    else
+                    {
+                        nextRouter = master.getRightConnection(id);
+                        connectOut = new Socket(master.getIp(nextRouter), 4446); // attempts to ring the bell of this socket address
+                    }
 
-                PrintWriter out = new PrintWriter(connectOut.getOutputStream(), true);// sends out to client
+                    PrintWriter out = new PrintWriter(connectOut.getOutputStream(), true);// sends out to client
 
-                out.println(message); // sends to client's own server, to be sent out to another server
+                    out.println(message); // sends to client's own server, to be sent out to another server
+                }
 
                 connect.close();
                 router.run();
@@ -71,4 +74,59 @@ import java.net.Socket;
                 e.printStackTrace();
             }
         }
+        
+        /**
+    	 * Ensures the checksum is good, if it isn't it returns false
+    	 * @param message
+    	 * @return
+    	 * @author Jessica Schlesiger
+    	 */
+    	static boolean checkChecksum(String message)
+    	{
+    		int locationOfChecksum = 2;
+    		String checksum = addLeadingZeros(Integer.toBinaryString(message.charAt(locationOfChecksum)));
+
+    		StringBuilder msg = new StringBuilder(message);
+    		message = msg.deleteCharAt(locationOfChecksum).toString(); // deletes the checksum character
+    		char[] chars= new char[4];
+    		for (int i=0;i<4;i++) // grabs each character individually
+    		{
+    			//System.out.println(addLeadingZeros(Integer.toBinaryString((char)message.charAt(i))));
+    			chars[i] = message.charAt(i);
+    		}
+    		String charSum=Integer.toBinaryString(chars[0]+chars[1]+chars[2]+chars[3]);
+    		if (charSum.length() > 8) // carries the 1
+    		{
+    			charSum = Integer.toBinaryString((chars[0]+chars[1]+chars[2]+chars[3])+1);
+    			charSum = addLeadingZeros(charSum.substring(1,8));
+    		}
+//    		System.out.println(charSum);
+//    		System.out.println(checksum);
+
+    		for (int i=0;i<8;i++)
+    		{
+    			if (charSum.charAt(i) != '1')
+    				if (checksum.charAt(i) != '1')
+    					return false;
+    		}
+    		return true;
+    	}
+    	
+    	/**
+    	 * adds the leading zeros to our binary value
+    	 * @param binaryString
+    	 * @return the binary string with leading zeros
+    	 * @author Jessica Schlesiger
+    	 */
+    	static String addLeadingZeros(String binaryString)
+    	{
+    		int leadingZeros = 8-binaryString.length();
+    		String zeros="";
+    		for (int i=0;i<leadingZeros;i++)
+    		{
+    			zeros+='0';
+    		}
+    		return zeros+binaryString;
+    	}
+        
     }
